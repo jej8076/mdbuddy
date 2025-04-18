75601a91b8c82c8f878fdf1d5c7d7aec3eacb0a7
jej8076 <jej8076@gmail.com>
2025-04-18 16:01:59 +0900

    파일 열기, 저장 등 구현 중

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mdbuddy/screens/markdown_editor/martdown_editor_screen.dart';
+import 'package:mdbuddy/services/file_service.dart';
 import 'themes/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: MarkdownEditor(),
+
+    String? _currentFilePath; // 현재 열려있는 파일 경로를 저장
+
+    final FileService fileService = FileService();
+
+    // void _pickFile() async {
+    //   final filePath = await _fileService.pickFile();
+    //   if (filePath != null) {
+    //     final content = await _fileService.readFile(filePath);
+    //     setState(() {
+    //       _controller.text = content;
+    //       _markdownData = content;
+    //       _currentFilePath = filePath;
+    //     });
+    //   }
+    // }
+
+    // void _saveAsFile() async {
+    //   final filePath = await _fileService.saveAsFile(_controller.text);
+    //   if (filePath != null) {
+    //     setState(() {
+    //       _currentFilePath = filePath;
+    //     });
+    //     _showSuccessSnackBar("파일이 성공적으로 저장되었습니다.");
+    //   }
+    // }
+
+    // void _showSuccessSnackBar(String message) {
+    //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    //     content: Text(message),
+    //     backgroundColor: Colors.green,
+    //   ));
+    // }
+    //
+    // void _showErrorSnackBar(String message) {
+    //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    //     content: Text(message),
+    //     backgroundColor: Colors.red,
+    //   ));
+    // }
+
+    return PlatformMenuBar(
+      menus: [
+        PlatformMenu(
+          label: 'App',
+          menus: [
+            // App 메뉴 항목들
+          ],
+        ),
+        PlatformMenu(
+          label: 'File',
+          menus: [
+            PlatformMenuItem(
+              label: 'New',
+              shortcut: const SingleActivator(LogicalKeyboardKey.keyN, meta: true),
+              onSelected: () {
+                print('New menu item selected');
+              },
+            ),
+            // 기타 File 메뉴 항목들
+          ],
+        ),
+        PlatformMenu(
+          label: 'Edit',
+          menus: [
+            // Edit 메뉴 항목들
+          ],
+        ),
+        PlatformMenu(
+          label: 'View',
+          menus: [
+            // View 메뉴 항목들
+          ],
+        ),
+        // 기타 필요한 메뉴들
+      ],
+      child: MaterialApp(
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: MarkdownEditor(),
+      ),
     );
   }
 }
--- lib/screens/markdown_editor/markdown_editor_state.dart
+++ lib/screens/markdown_editor/markdown_editor_state.dart
@@ -5,80 +5,14 @@ import 'package:mdbuddy/screens/markdown_editor/martdown_editor_screen.dart';
 import 'package:window_manager/window_manager.dart';
 
 import '../../main.dart';
-import '../../services/file_service.dart';
 import '../../utils/key_handler.dart';
 import 'components/markdown_input.dart';
 
 class MarkdownEditorState extends State<MarkdownEditor> {
   double _dragPosition = 100.0;
+
   final TextEditingController _controller = TextEditingController();
-  String _markdownData = "";
-  bool _isVertical = true; // true: 위아래 모드, false: 좌우 모드
-  bool _isViewerMode = false; // true: Viewer 모드 (텍스트 입력 숨김)
-  String? _currentFilePath; // 현재 열려있는 파일 경로를 저장
   final KeyHandler _keyHandler = KeyHandler();
-  final FileService _fileService = FileService();
-
-  void _toggleLayout() {
-    setState(() {
-      _isVertical = !_isVertical;
-    });
-  }
-
-  void _toggleViewerMode() {
-    setState(() {
-      _isViewerMode = !_isViewerMode;
-    });
-  }
-
-  void _pickFile() async {
-    final filePath = await _fileService.pickFile();
-    if (filePath != null) {
-      final content = await _fileService.readFile(filePath);
-      setState(() {
-        _controller.text = content;
-        _markdownData = content;
-        _currentFilePath = filePath;
-      });
-    }
-  }
-
-  void _saveFile() async {
-    if (_currentFilePath != null) {
-      final success = await _fileService.saveFile(_currentFilePath!, _controller.text);
-      if (success) {
-        _showSuccessSnackBar("파일이 성공적으로 저장되었습니다.");
-      } else {
-        _showErrorSnackBar("파일 저장 중 오류가 발생했습니다.");
-      }
-    } else {
-      _saveAsFile();
-    }
-  }
-
-  void _saveAsFile() async {
-    final filePath = await _fileService.saveAsFile(_controller.text);
-    if (filePath != null) {
-      setState(() {
-        _currentFilePath = filePath;
-      });
-      _showSuccessSnackBar("파일이 성공적으로 저장되었습니다.");
-    }
-  }
-
-  void _showSuccessSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(message),
-      backgroundColor: Colors.green,
-    ));
-  }
-
-  void _showErrorSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(message),
-      backgroundColor: Colors.red,
-    ));
-  }
 
   @override
   void initState() {
@@ -139,15 +73,6 @@ class MarkdownEditorState extends State<MarkdownEditor> {
             ),
           ),
         ),
-        // appBar: EditorAppBar(
-        //   pickFile: _pickFile,
-        //   saveFile: _saveFile,
-        //   saveAsFile: _saveAsFile,
-        //   toggleLayout: _toggleLayout,
-        //   toggleViewerMode: _toggleViewerMode,
-        //   isVertical: _isVertical,
-        //   isViewerMode: _isViewerMode,
-        // ),
         body: BlocBuilder<MarkdownLineStyleBloc, MarkdownLineStyleState>(
           builder: (context, state) {
             final lineStyles = state.lineStyles;
@@ -160,13 +85,8 @@ class MarkdownEditorState extends State<MarkdownEditor> {
                     onChanged: (text) {
                       setState(() {});
                     },
-                    hintText: '마크다운을 입력하세요...',
                   ),
                 ),
-                // Divider(height: 2, thickness: 2),
-                // Expanded(
-                //   child: MarkdownPreview(markdownData: _markdownData),
-                // ),
               ],
             );
           },
